428a3345c0dc6c1ce359283d28e414b2ae9c2b52
smoisset <smoisset@worldremit.com>
2017-11-26 12:21:42 +0000

    Add S06.01-Exercise-LaunchSettingsActivity

---
+++ Sunshine/S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_settings);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
